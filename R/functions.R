8af930e01ad6985e1e776519be35006a62bad964
Daniel Anderson <daniela@uoregon.edu>
2020-09-25 15:27:43 -0700

    Use all data, create time-series plot of mentions

--- R/functions.R
+++ R/functions.R
@@ -1,23 +1,44 @@
 #d <- tar_read(raw_data)
+
 prep_data <- function(data) {
   data %>% 
     clean_names() %>% 
-    filter(overperforming_score != min(overperforming_score, na.rm = TRUE))
+    mutate(created = lubridate::as_datetime(created))
 }
 
-create_hist_perform <- function(data) {
-  p <- ggplot(data, aes(overperforming_score)) +
-    geom_histogram(fill = "cornflowerblue",
-                   alpha = 0.7) +
-    theme_minimal(15) +
-    labs(title = "Preliminary plot",
-         y = "Count") +
+compute_covid_mentions <- function(data) {
+  data %>% 
+    mutate(message = tolower(message),
+           description = tolower(description),
+           link_text = tolower(link_text),
+           covid_mentioned = grepl("covid|coronavirus", message) |
+             grepl("covid|coronavirus", description) |
+             grepl("covid|coronavirus", link_text),
+           month = month(created),
+           day = day(created)) %>% 
+    group_by(month, day) %>% 
+    summarize(covid_mentions = sum(covid_mentioned)) %>% 
+    mutate(date = make_date(year = 2020, month = month, day = day)) %>% 
+    ungroup() %>% 
+    mutate(weekday = weekdays(date)) %>% 
+    filter(!weekday %in% c("Saturday", "Sunday")) %>% 
+    select(date, covid_mentions)
+}
+
+create_covid_mention_plot <- function(covid_mention_data) {
+  p <- ggplot(covid_mention_data, aes(date, covid_mentions)) +
+    geom_line(color = "cornflowerblue",
+              size = 1.2) +
+    labs(title = "Mentions of COVID or Coronavirus",
+         subtitle = "Data from weekends removed",
+         y = "COVID Mention Count") +
+    theme_minimal() +
     theme(plot.title.position = "plot")
   
-  ggsave(here::here("plots", "hist_perform.png"), 
+  ggsave(here("plots", "covid-mentions.png"), 
          p,
          width = 6.5,
          height = 8)
   
-  here::here("plots", "hist_perform.png")
+  here("plots", "covid-mentions.png")
 }
--- _targets.R
+++ _targets.R
@@ -3,18 +3,21 @@ library(targets)
 source(here::here("R", "functions.R"))
 
 # Set target-specific options such as packages.
-tar_option_set(packages = c("readr", "ggplot2", "janitor", "dplyr")
+tar_option_set(packages = c("purrr", "readr", "ggplot2", "janitor", "dplyr",
+                            "here", "lubridate")
                )
 
 # Define targets
 targets <- list(
-  tar_target(file, list.files(here::here("data"), 
+  tar_target(files, list.files(here::here("data"), 
                               full.names = TRUE,
                               pattern = "\\.csv$"), 
              format = "file"),
-  tar_target(raw_data, read_csv(file)),
+  tar_target(raw_data, map_df(files, read_csv)),
   tar_target(d, prep_data(raw_data)),
-  tar_target(hist_perform, create_hist_perform(d), format = "file")
+  tar_target(covid_mentions, compute_covid_mentions(d)),
+  tar_target(plot_covid_mentions, create_covid_mention_plot(covid_mentions), 
+             format = "file")
 )
 
 # End with a call to tar_pipeline() to wrangle the targets together.
